d3835bcbd515dd5abcb78bbd93b44dd93e1fdcb4
Alex Berry <Alexanderberry@mail.weber.edu>
2025-11-08 13:29:07 -0700

    Added debugging logs to wsgi.py

--- portfolio_site/wsgi.py
+++ portfolio_site/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_site.settings')
-
+print("WSGI starting…", file=sys.stderr)
+print("DATABASE_URL:", os.environ.get("DATABASE_URL"), file=sys.stderr)
+print("ALLOWED_HOSTS:", os.environ.get("ALLOWED_HOSTS"), file=sys.stderr)
 application = get_wsgi_application()
